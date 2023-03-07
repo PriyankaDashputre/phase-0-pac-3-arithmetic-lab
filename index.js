9ce196b405bcc7eae8850e6cc7e90cbc58673cc7
PriyankaDashputre <dashputre.priyanka@gmail.com>
2023-03-06 18:18:17 -0800

    Math function lab

---
+++ index.js
@@ -0,0 +1,28 @@
+function add() {}
+function subtract() {}
+function multiply() {}
+function divide() {}
+function add (a, b){
+    return a + b;
+}
+function subtract (a, b){
+    return a - b;
+}
+function multiply (a, b){
+    return a * b;
+}
+function divide (a, b){
+    return a / b;
+}
+function increment (a) {
+return ++a;
+}
+function decrement (a) {
+return --a;
+}
+function makeInt(a) {
+return parseInt(a.toString(), 10);
+}
+function preserveDecimal(a) {
+return parseFloat (a);
+}
